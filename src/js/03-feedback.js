ed02c6daebd30e4816bddf88837b533a532bdaf1
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2021-12-22 22:35:59 +0200

    fix reading from local storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,19 +4,20 @@ const refs = {
     input: document.querySelector('input')
 }
 
-let buffer = {};
-function onInputType(e) {
-    buffer[e.target.name] = e.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(buffer));
 
-}
-buffer = localStorage.getItem("feedback-form-state");
+let buffer = localStorage.getItem("feedback-form-state");
 if (buffer) {
     buffer = JSON.parse(buffer);
     refs.input.value = buffer.email;
     refs.textArea.value = buffer.message;
 }
 
+buffer = {};
+function onInputType(e) {
+    buffer[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(buffer));
+
+}
 
 refs.feedbackForm.addEventListener('input', onInputType);
 
