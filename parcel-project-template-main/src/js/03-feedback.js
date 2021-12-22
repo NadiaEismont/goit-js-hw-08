22bac5af518062a3b5f4ae9803136704b8da4b14
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2021-12-22 21:51:35 +0200

    task3

--- parcel-project-template-main/src/js/03-feedback.js
+++ parcel-project-template-main/src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const refs = {
+    feedbackForm: document.querySelector('.feedback-form'),
+    textArea: document.querySelector('textarea'),
+    input: document.querySelector('input')
+}
+
+let buffer = {};
+function onInputType(e) {
+    buffer[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(buffer));
+
+}
+buffer = localStorage.getItem("feedback-form-state");
+if (buffer) {
+    buffer = JSON.parse(buffer);
+    refs.input.value = buffer.email;
+    refs.textArea.value = buffer.message;
+}
+
+
+refs.feedbackForm.addEventListener('input', onInputType);
+
 
